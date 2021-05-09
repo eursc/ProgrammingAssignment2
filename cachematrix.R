4124813b45242aa5de903e96274cceb212e951c4
eursc <72342227+eursc@users.noreply.github.com>
2021-05-09 18:40:40 +0300

    Add files via upload
    r script for assignment - 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## the input x as a matrix
+## and then set the solved value "sol" as a null
+## then "mean" standard function to "solve" standard function
+#1. set the value of the matrix 2. get the value of the matrix 
+#3. set the value of inverse of the matrix 4. get the value of inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  sol <- NULL
+  set <- function(y) {
+    x <<- y
+    sol <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) sol <<- solve
+  getsolve <- function() sol
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+##
+## "mean" to "solve", "m" to "sol"
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  sol <- x$getsolve()
+  if(!is.null(sol)) {
+    message("getting inversed matrix")
+    return(sol)
+  }
+  data <- x$get()
+  sol <- solve(data, ...)
+  x$setsolve(sol)
+  sol
 }
+
+
